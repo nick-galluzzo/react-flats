c17122f96bbaf1633999bf3ea756dd4c4aa8dfa7
Nick Galluzzo <n.galluzzo@gmail.com>
2019-12-31 15:25:16 -0500

    added store provider

--- src/index.jsx
+++ src/index.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { Provider } from 'react-redux';
 import ReactDOM from 'react-dom';
 import App from './components/app';
+import store from './components/store';
 
 import '../assets/stylesheets/application.scss';
 
 const root = document.getElementById('root');
 if (root) {
-  ReactDOM.render(<App />, root);
+  ReactDOM.render(
+    <Provider store={store}>
+      <App />
+    </Provider>,
+    root
+  );
 }
