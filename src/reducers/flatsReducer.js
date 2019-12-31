2aee2115dfefc227de2050cb007a5ab33532c0de
Nick Galluzzo <n.galluzzo@gmail.com>
2019-12-31 17:51:12 -0500

    return empty array if state is undefined clause

--- src/reducers/flatsReducer.js
+++ src/reducers/flatsReducer.js
@@ -1,6 +1,10 @@
 import { LIST_FLATS } from '../actions/flatActions';
 
 const flatsReducer = (state = {}, action) => {
+  if (state === undefined) {
+    return [];
+  }
+
   switch (action.type) {
     case LIST_FLATS:
       return action.payload;
